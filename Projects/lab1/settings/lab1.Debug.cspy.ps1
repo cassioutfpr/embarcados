48bf9a10919403fb1a50c20a932310e3d525b5e3
Luiz Agner <luizagnern@hotmail.com>
2019-10-04 22:08:12 -0300

    Adding lab 2 project files

--- Projects/lab1/settings/lab1.Debug.cspy.ps1
+++ Projects/lab1/settings/lab1.Debug.cspy.ps1
@@ -23,9 +23,9 @@
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "C:\Users\luiza\OneDrive\UTFPR\Arquivos\2019-2\Sistemas Embarcados\Labs\embarcados\Projects\lab1\settings\lab1.Debug.general.xcl" --backend -f "C:\Users\luiza\OneDrive\UTFPR\Arquivos\2019-2\Sistemas Embarcados\Labs\embarcados\Projects\lab1\settings\lab1.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "D:\Embarcados\embarcados\Projects\lab1\settings\lab1.Debug.general.xcl" --backend -f "D:\Embarcados\embarcados\Projects\lab1\settings\lab1.Debug.driver.xcl" 
 }
 else
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "C:\Users\luiza\OneDrive\UTFPR\Arquivos\2019-2\Sistemas Embarcados\Labs\embarcados\Projects\lab1\settings\lab1.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\luiza\OneDrive\UTFPR\Arquivos\2019-2\Sistemas Embarcados\Labs\embarcados\Projects\lab1\settings\lab1.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "D:\Embarcados\embarcados\Projects\lab1\settings\lab1.Debug.general.xcl" --debug_file=$debugfile --backend -f "D:\Embarcados\embarcados\Projects\lab1\settings\lab1.Debug.driver.xcl" 
 }
--- TivaWare_C_Series-2.1.4.178/driverlib/settings/driverlib.Debug.cspy.bat
+++ TivaWare_C_Series-2.1.4.178/driverlib/settings/driverlib.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "C:\Users\luiza\OneDrive\UTFPR\Arquivos\2019-2\Sistemas Embarcados\Labs\embarcados\TivaWare_C_Series-2.1.4.178\driverlib\settings\driverlib.Debug.general.xcl" --backend -f "C:\Users\luiza\OneDrive\UTFPR\Arquivos\2019-2\Sistemas Embarcados\Labs\embarcados\TivaWare_C_Series-2.1.4.178\driverlib\settings\driverlib.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "D:\Embarcados\embarcados\TivaWare_C_Series-2.1.4.178\driverlib\settings\driverlib.Debug.general.xcl" --backend -f "D:\Embarcados\embarcados\TivaWare_C_Series-2.1.4.178\driverlib\settings\driverlib.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "C:\Users\luiza\OneDrive\UTFPR\Arquivos\2019-2\Sistemas Embarcados\Labs\embarcados\TivaWare_C_Series-2.1.4.178\driverlib\settings\driverlib.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\luiza\OneDrive\UTFPR\Arquivos\2019-2\Sistemas Embarcados\Labs\embarcados\TivaWare_C_Series-2.1.4.178\driverlib\settings\driverlib.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "D:\Embarcados\embarcados\TivaWare_C_Series-2.1.4.178\driverlib\settings\driverlib.Debug.general.xcl" "--debug_file=%~1" --backend -f "D:\Embarcados\embarcados\TivaWare_C_Series-2.1.4.178\driverlib\settings\driverlib.Debug.driver.xcl" 
 
 @echo off 
 :end
