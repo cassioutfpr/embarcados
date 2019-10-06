8b5acd5d8353913f8ed9a9b043c3e5d7a813ef2e
Cassio Morales <cassioutfpr@gmail.com>
2019-10-05 22:15:06 -0300

    Major changes made to main file.

--- Projects/lab2/settings/lab2.Debug.cspy.ps1
+++ Projects/lab2/settings/lab2.Debug.cspy.ps1
@@ -23,9 +23,9 @@
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "D:\Embarcados\embarcados\Projects\lab2\settings\lab2.Debug.general.xcl" --backend -f "D:\Embarcados\embarcados\Projects\lab2\settings\lab2.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "C:\Users\Caio\Desktop\embarcados\embarcados\Projects\lab2\settings\lab2.Debug.general.xcl" --backend -f "C:\Users\Caio\Desktop\embarcados\embarcados\Projects\lab2\settings\lab2.Debug.driver.xcl" 
 }
 else
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "D:\Embarcados\embarcados\Projects\lab2\settings\lab2.Debug.general.xcl" --debug_file=$debugfile --backend -f "D:\Embarcados\embarcados\Projects\lab2\settings\lab2.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "C:\Users\Caio\Desktop\embarcados\embarcados\Projects\lab2\settings\lab2.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\Caio\Desktop\embarcados\embarcados\Projects\lab2\settings\lab2.Debug.driver.xcl" 
 }
--- Projects/lab2/src/mainLab2.c
+++ Projects/lab2/src/mainLab2.c
@@ -1,7 +1,6 @@
 // S12 - G03 - Lab1
 // Cássio Morales - 1612239
 // Luiz Agner - 1612280
-
 #include <stdint.h>
 #include <stdbool.h>
 // includes da biblioteca driverlib
@@ -10,6 +9,7 @@
 #include "driverlib/sysctl.h"
 #include "driverlib/systick.h"
 #include "driverlib/timer.h"
+#define TIME_OUT 1000000
 
 uint8_t LED_D1 = 0;
 
@@ -18,41 +18,60 @@ enum states{
   LOW  = 1
 };
 
-bool high = true;
+bool high = false;
+bool high2 = false;
+volatile int time_on_high = 0;
+volatile int time_on_low = 0;
 
-void timeOutHandler(void)
-{
-  if(high)
+void handler2(void){
+  if(high2)
   {
-    //TODO timeout on high
-  }else
-  {
-    //TODO timeout on low
+     GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_PIN_4); // Apaga LED D4
+     time_on_high = 16000000;
+     high2 = false;
   }
-}
+  else{
+     GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_4, 0); // Apaga LED D4
+    time_on_low = 16000000;
+    high2 = true;
+  }
+  
+  SysTickIntDisable();
+  SysTickDisable();
+  SysTickPeriodSet(12000000);
+  SysTickIntEnable();
+  SysTickEnable();
+  SysTickIntRegister(handler2);
+  //MANDAR PRA UART
+    
+} // SysTick_Handler
 
 void handlerEntrancePwm(void)
 {
-  if(high){
-    GPIOIntTypeSet(GPIO_PORTE_BASE, GPIO_PIN_0, GPIO_FALLING_EDGE);
-    high = false;
-    int timeOnHigh = TimerValueGet(TIMER0_BASE, TIMER_A);
-    TimerDisable(TIMER0_BASE, TIMER_A);
-    //Verificar se disable ou outra função dá clear no TIMER_A
-    TimerLoadSet(TIMER0_BASE, TIMER_A, 3000);
-    TimerEnable(TIMER0_BASE, TIMER_A);
-  }
-  else
-  {
-     GPIOIntTypeSet(GPIO_PORTE_BASE, GPIO_PIN_0, GPIO_RISING_EDGE);
-     high = true;
-     int timeOnHigh = TimerValueGet(TIMER0_BASE, TIMER_A);
-     TimerDisable(TIMER0_BASE, TIMER_A);
-     //Verificar se disable ou outra função dá clear no TIMER_A
-     TimerLoadSet(TIMER0_BASE, TIMER_A, 3000);
-     TimerEnable(TIMER0_BASE, TIMER_A);
-  }
-  
+  SysTickIntDisable();
+  SysTickDisable();
+  SysTickPeriodSet(12000000); // f = 1Hz para clock = 24MHz
+  SysTickIntRegister(handler2);
+  SysTickIntEnable();
+  SysTickEnable();
+   volatile int loco = 0;
+   if(high) // Testa estado do push-button SW2
+   {
+      GPIOIntTypeSet(GPIO_PORTE_BASE, GPIO_PIN_0, GPIO_FALLING_EDGE);
+      GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, 1); // Apaga LED D4
+      high = false;
+      loco = SysTickValueGet();
+      loco++;
+      time_on_low = SysTickValueGet();
+   }
+   else
+   {
+      GPIOIntTypeSet(GPIO_PORTE_BASE, GPIO_PIN_0, GPIO_RISING_EDGE);
+      GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, 0); // Acende LED D4
+      high = true;
+      time_on_high = SysTickValueGet();
+   }
+  GPIOIntClear(GPIO_PORTE_BASE, GPIO_INT_PIN_0);
 }
 
 void gpio_initialization()
@@ -60,36 +79,26 @@ void gpio_initialization()
   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE); // Habilita GPIO E
   while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOE)); // Aguarda final da habilitação
   GPIOPinTypeGPIOInput(GPIO_PORTE_BASE, GPIO_PIN_0); // Pino 0 como entrada
-  
-  GPIOIntRegister(GPIO_PORTE_BASE, handlerEntrancePwm);
-  
   GPIOIntTypeSet(GPIO_PORTE_BASE, GPIO_PIN_0, GPIO_RISING_EDGE);
-  //GPIO_FALLING_EDGE; GPIO_BOTH_EDGES, GPIO_LOW_LEVEL, GPIO_HIGH_LEVEL
-  
+  GPIOIntRegister(GPIO_PORTE_BASE, handlerEntrancePwm);
   GPIOIntEnable(GPIO_PORTE_BASE, GPIO_INT_PIN_0);
+
 }
 
 void main(void){
-  //// Enable the Timer0 peripheral//
-  SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
-  //// Wait for the Timer0 module to be ready.//
-  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER0)){}
+
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); // Habilita GPIO F (LED D3 = PF4, LED D4 = PF0)
+  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)); // Aguarda final da habilitação
+    
+  GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_0 | GPIO_PIN_4); // LEDs D3 e D4 como saída
+  GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0 | GPIO_PIN_4, 0); // LEDs D3 e D4 apagados
+  GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_0 | GPIO_PIN_4, GPIO_STRENGTH_12MA, GPIO_PIN_TYPE_STD);
   
-  /* FUNCTIONS TO BE USED LATER
-  TimerConfigure(TIMER0_BASE, TIMER_CFG_ONE_SHOT_UP);
-  
-  TimerControlEvent(TIMER0_BASE, TIMER_A, TIMER_EVENT_POS_EDGE);
-  
-  TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
-  
-  TimerEnable(TIMER0_BASE, TIMER_A);
-  
-  TimerValueGet(TIMER0_BASE, TIMER_A);
-  
-  TimerIntRegister(TIMER0_BASE, TIMER_A, timeOutHandler);
-  
-  TimerDisable(uint32_t ui32Base,uint32_t ui32Timer);
-  */
   //UARTInit();
   gpio_initialization();
+  int opalele = 0;
+  while(1){
+    opalele++;
+    opalele--;
+  }
 } // main
