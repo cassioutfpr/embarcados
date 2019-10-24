8cc0996d938e588b73107943e104cf41d91695d3
Luiz Agner <luizagnern@hotmail.com>
2019-10-24 20:00:27 -0300

    Implementing PWM reader with GPTM

--- Projects/lab2/src/mainLab2.c
+++ Projects/lab2/src/mainLab2.c
@@ -28,10 +28,14 @@ enum states{
   LOW  = 1
 };
 
-bool high = false;
+bool high = true;
+bool firstTime = true;
+bool timeout = false;
 int timeSpentOnHigh = 0;
 int timeSpentOnLow = 0;
 int timeSpentUntilLastPrint = 0;
+int startTime = 0;
+int endTime = 0;
 
 void UARTInit(void){
   // Enable the GPIO Peripheral used by the UART.
@@ -49,7 +53,7 @@ void UARTInit(void){
 
   // Initialize the UART for console I/O.
   UARTStdioConfig(0, 100000, SystemCoreClock);
-} // UARTInit
+}
 
 void resetSysTick(void)
 {
@@ -60,12 +64,7 @@ void resetSysTick(void)
 
 void UART0_Handler(void){
   UARTStdioIntHandler();
-} // UART0_Handler
-
-void timeOutHandler(void)
-{
-  UARTprintf("TIMEOUT\n"); 
-} // SysTick_Handler
+}
 
 void sendUart(int period, float frequency, float dutyCycle)
 {
@@ -80,7 +79,7 @@ void sendUart(int period, float frequency, float dutyCycle)
   decimal_duty_cycle = (int)((dutyCycle - (float)integer_duty_cycle)*10);
   
   UARTprintf("Periodo %d us\nFrequencia %d.%d%d Hz\nDuty Cycle %d.%d\n", period, integer_frequency, decimal_frequency, centesimal_frequency, integer_duty_cycle, decimal_duty_cycle);
-} //sendUart
+}
 
 void computeParametersAndSendToUART()
 {
@@ -91,35 +90,55 @@ void computeParametersAndSendToUART()
   sendUart(periodus, frequency, duty_cycle*100.0);
 }
 
-void handlerEntrancePwm(void)
+void reset_timer() 
 {
-   volatile int local;  
-   if(high) 
-   {
-      GPIOIntTypeSet(GPIO_PORTD_BASE, GPIO_PIN_0, GPIO_FALLING_EDGE);
-      high = false;
-      timeSpentOnLow = ((TIME_OUT - SysTickValueGet()))/CLOCK_US;
-   }
-   else
-   {
-      GPIOIntTypeSet(GPIO_PORTD_BASE, GPIO_PIN_0, GPIO_RISING_EDGE);
-      high = true;
-      timeSpentOnHigh = ((TIME_OUT - SysTickValueGet()))/CLOCK_US;
-   }
-   
-   local = timeSpentOnLow;
-  if(timeSpentUntilLastPrint >= TIME_SPENT_TO_PRINT)
-  {
-    computeParametersAndSendToUART();
-    timeSpentUntilLastPrint = 0;
-  }
-  GPIOIntClear(GPIO_PORTD_BASE, GPIO_INT_PIN_0);
-  resetSysTick();
+  TimerIntClear(TIMER0_BASE, TIMER_CAPA_EVENT);
+  TimerIntClear(TIMER0_BASE, TIMER_TIMB_TIMEOUT);
+  TimerLoadSet(TIMER0_BASE, TIMER_A, 0xFFFF);
+  TimerLoadSet(TIMER0_BASE, TIMER_B, 0xFFFF);
 }
 
 void handler_timers(void)
 {
+  if(high)
+  {
+    timeSpentOnLow = (65535 - TimerValueGet(TIMER0_BASE, TIMER_A))/CLOCK_US;
+    high = false;
+    TimerControlEvent(TIMER0_BASE, TIMER_A, TIMER_EVENT_NEG_EDGE);
+    reset_timer();
+  }
+  else
+  {
+    timeSpentOnHigh = (65535 - TimerValueGet(TIMER0_BASE, TIMER_A))/CLOCK_US;
+    high = true;
+    TimerControlEvent(TIMER0_BASE, TIMER_A, TIMER_EVENT_POS_EDGE);
+    reset_timer();
+  }
+    
+  if(!timeout) 
+  {
+    if(timeSpentUntilLastPrint >= TIME_SPENT_TO_PRINT)
+    {
+      computeParametersAndSendToUART();
+      timeSpentUntilLastPrint = 0;
+    }
+  }
   
+  timeout = false;
+}
+
+void handler_timeout(void){
+  if(timeSpentUntilLastPrint >= TIME_SPENT_TO_PRINT)
+  {
+    if(!high) {
+      UARTprintf("Periodo infinito us\nFrequencia 0 Hz\nDuty Cycle 100\n");
+    } else {
+      UARTprintf("Periodo infinito us\nFrequencia 0 Hz\nDuty Cycle 0\n");
+    }
+    timeSpentUntilLastPrint = 0;
+  }
+  reset_timer();
+  timeout = true;
 }
 
 void gpio_initialization()
@@ -127,34 +146,26 @@ void gpio_initialization()
   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD); 
   while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOD)); 
   
-  GPIOPinTypeTimer(GPIO_PORTD_BASE, GPIO_PIN_0);//Configures pin(s) for use by the Timer peripheral.
-    //#1 parameter: the base address of the GPIO port.
-    //#2parameter: the bit-packed representation of the pin(s)
+  GPIOPinTypeTimer(GPIO_PORTD_BASE, GPIO_PIN_0);
   
-  GPIOPinConfigure(GPIO_PD0_T0CCP0); // Configures the alternate function of a GPIO pin.
-      
-  //GPIOPinTypeGPIOInput(GPIO_PORTD_BASE, GPIO_PIN_0);
-  //GPIOIntTypeSet(GPIO_PORTD_BASE, GPIO_PIN_0, GPIO_RISING_EDGE);
-  //GPIOIntRegister(GPIO_PORTD_BASE, handlerEntrancePwm);
-  //GPIOIntEnable(GPIO_PORTD_BASE, GPIO_INT_PIN_0);
+  GPIOPinConfigure(GPIO_PD0_T0CCP0);
 }
 
 void timer_initialization(void)
 {
   SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
   while(!SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER0));
-  TimerConfigure(TIMER0_BASE, (TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_CAP_TIME_UP | TIMER_CFG_B_CAP_TIME_UP));
-  TimerControlEvent(TIMER0_BASE, TIMER_BOTH, TIMER_EVENT_POS_EDGE);
-  //TIMER_EVENT_POS_EDGE
-  //TIMER_EVENT_NEG_EDGE
-  //TIMER_EVENT_BOTH_EDGES
+  TimerConfigure(TIMER0_BASE, (TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_CAP_TIME | TIMER_CFG_B_PERIODIC));
+  TimerControlEvent(TIMER0_BASE, TIMER_A, TIMER_EVENT_POS_EDGE);
   
-  //TimerLoadSet(TIMER0_BASE, TIMER_BOTH, 0xFFFF);
-  TimerEnable(TIMER0_BASE, TIMER_BOTH);
-  TimerIntClear(TIMER0_BASE, TIMER_CAPA_EVENT | TIMER_CAPB_EVENT);
-  TimerIntEnable(TIMER0_BASE, TIMER_CAPA_EVENT | TIMER_CAPB_EVENT);
-  TimerIntRegister(TIMER0_BASE, TIMER_BOTH, handler_timers);
+  TimerLoadSet(TIMER0_BASE, TIMER_B, 0xFFFF);
+  TimerIntEnable(TIMER0_BASE, TIMER_CAPA_EVENT | TIMER_TIMB_TIMEOUT);
+  TimerIntRegister(TIMER0_BASE, TIMER_A, handler_timers);
+  TimerIntRegister(TIMER0_BASE, TIMER_B, handler_timeout);
+  TimerEnable(TIMER0_BASE, TIMER_A);
+  TimerEnable(TIMER0_BASE, TIMER_B);
 }
+
 void main(void)
 {
   UARTInit();
@@ -165,4 +176,4 @@ void main(void)
     timeSpentUntilLastPrint++;
     SysCtlDelay(INSTRUCTIONS_PER_MICROSECONDS*LOOP_DELAY_IN_MICROSECONDS);
   }
-} // main
+}
